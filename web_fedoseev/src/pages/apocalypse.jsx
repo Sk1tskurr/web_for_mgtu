489ab84185e86ef4d6e63a206fab3d8da3f70e3d
Fedoseev Aleksey <zloystrag@gmail.com>
2024-10-10 22:34:28 +0300

    Починиль апокалипсис

--- web_fedoseev/src/pages/apocalypse.jsx
+++ web_fedoseev/src/pages/apocalypse.jsx
@@ -1,13 +1,18 @@
+// web_fedoseev/src/pages/apocalypse.jsx
+
 import React from 'react';
-import './Apocalypse.css'; // Подключаем CSS файл для стилей
+import './Apocalypse.css'; // стили
 
 const Apocalypse = () => {
     return (
         <div className="apocalypse-container">
-            <video className="apocalypse-video" autoPlay loop muted>
-                <source src="../images/apocalypse.mp4" type="video/mp4" />
-                Ваш браузер не поддерживает видео.
+            <video autoPlay loop muted className="apocalypse-video">
+                <source src={require('../images/apocalypse.mp4')} type="video/mp4" />
+                Ваш браузер не поддерживает воспроизведение видео.
             </video>
+            <div className="apocalypse-text">
+                <h1>Это был осознанный выбор</h1>
+            </div>
         </div>
     );
 };
